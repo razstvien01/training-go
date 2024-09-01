090302b264e9d4e12d28cfc942072b82cf593777
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-09-01 16:35:15 +0800

    added GET, POST, and DELETE request in the main

--- basics/main.go
+++ basics/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +22,23 @@ func main(){
 		})
 	})
 	
+	e.POST("/todos", func(c *gin.Context){
+		title := c.PostForm("title")
+		status := c.PostForm("status")
+		id, _ := CreateToDo(title, status)
+		
+		c.HTML(http.StatusOK, "task.html", gin.H{
+			"title": title,
+			"status": status,
+			"id": id,
+		})
+	})
+	
+	e.DELETE("/todos/:id", func(c *gin.Context) {
+		param := c.Param("id")
+		id, _ := strconv.ParseInt(param, 10, 64)
+		DeleteToDo(id)
+	})
 	
 	e.Run(":8080")
 }
